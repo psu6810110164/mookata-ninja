876190818e180515b2f572b6cffeb5f3d1d45a47
psu6810110164 <6810110164@psu.ac.th>
2026-02-23 17:29:19 +0700

    add all game system files

---
+++ main.py
@@ -0,0 +1,40 @@
+from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.lang import Builder
+from kivy.core.window import Window
+
+Window.size = (800, 450)
+
+Builder.load_file('mookata.kv')
+
+class MainMenuScreen(Screen):
+    pass
+
+class GameScreen(Screen):
+    pass
+
+class GameOverScreen(Screen):
+    pass
+
+class WindowManager(ScreenManager):
+    pass
+
+class MookataNinjaApp(App):
+    def build(self):
+        return WindowManager()
+
+class GameScreen(Screen):
+    def on_touch_down(self, touch):
+        print(f"เริ่มแตะที่พิกัด: {touch.pos}")
+        return super().on_touch_down(touch)
+
+    def on_touch_move(self, touch):
+        print(f"ลากนิ้วไปที่: {touch.pos}")
+        return super().on_touch_move(touch)
+
+    def on_touch_up(self, touch):
+        print("ปล่อยนิ้วแล้ว")
+        return super().on_touch_up(touch)
+
+if __name__ == '__main__':
+    MookataNinjaApp().run()
