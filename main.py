9f0ae38fb2795556b26101d1ffad0c7eb57a1558
psu6810110164 <6810110164@psu.ac.th>
2026-02-23 17:30:42 +0700

    add on_touch_down to detect start of slash

--- main.py
+++ main.py
@@ -10,8 +10,13 @@ Builder.load_file('mookata.kv')
 class MainMenuScreen(Screen):
     pass
 
-class GameScreen(Screen):
+class SettingsScreen(Screen):
     pass
+
+class GameScreen(Screen):
+    def on_touch_down(self, touch):
+        print(f"เริ่มแตะที่พิกัด: {touch.pos}")
+        return super().on_touch_down(touch)
 
 class GameOverScreen(Screen):
     pass
@@ -22,19 +27,6 @@ class WindowManager(ScreenManager):
 class MookataNinjaApp(App):
     def build(self):
         return WindowManager()
-
-class GameScreen(Screen):
-    def on_touch_down(self, touch):
-        print(f"เริ่มแตะที่พิกัด: {touch.pos}")
-        return super().on_touch_down(touch)
-
-    def on_touch_move(self, touch):
-        print(f"ลากนิ้วไปที่: {touch.pos}")
-        return super().on_touch_move(touch)
-
-    def on_touch_up(self, touch):
-        print("ปล่อยนิ้วแล้ว")
-        return super().on_touch_up(touch)
 
 if __name__ == '__main__':
     MookataNinjaApp().run()
