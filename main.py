a82a8738b2244c617516a654b48869f305988f15
psu6810110164 <6810110164@psu.ac.th>
2026-02-23 17:53:58 +0700

    test draw slash line

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
 from kivy.core.window import Window
+from kivy.graphics import Color, Line
 
 Window.size = (800, 450)
 
@@ -15,15 +16,19 @@ class SettingsScreen(Screen):
 
 class GameScreen(Screen):
     def on_touch_down(self, touch):
-        print(f"เริ่มแตะที่พิกัด: {touch.pos}")
+        with self.canvas:
+            Color(1, 0.4, 0, 1) 
+            touch.ud['slash_line'] = Line(points=(touch.x, touch.y), width=3)
         return super().on_touch_down(touch)
 
     def on_touch_move(self, touch):
-        print(f"ลากนิ้วไปที่: {touch.pos}")
+        if 'slash_line' in touch.ud:
+            touch.ud['slash_line'].points += [touch.x, touch.y]
         return super().on_touch_move(touch)
 
     def on_touch_up(self, touch):
-        print("ปล่อยนิ้วแล้ว")
+        if 'slash_line' in touch.ud:
+            self.canvas.remove(touch.ud['slash_line'])
         return super().on_touch_up(touch)
 
 class GameOverScreen(Screen):
