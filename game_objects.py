d93ea5d92c07edb7794b3fb4989d9c6f4ead0f56
psu6810110164 <6810110164@psu.ac.th>
2026-02-24 16:18:59 +0700

    Update physics to fit screen height and increase game speed

--- game_objects.py
+++ game_objects.py
@@ -1,25 +1,44 @@
+from math import sqrt
+from random import randint, choice
 from kivy.uix.image import Image
+from kivy.core.window import Window
 from kivy.graphics import PushMatrix, PopMatrix, Rotate
-from random import randint
 
 class FallingItem(Image):
-    def __init__(self, **kwargs):
+    def __init__(self, difficulty=1.0, is_bomb=False, **kwargs):
         super().__init__(**kwargs)
-        self.source = 'assets/images/heart_full.png'
-        self.size_hint = (None, None)
-        self.size = (100, 100)
-        self.x = randint(100, 700)
-        self.y = -100 
-        self.velocity_y = randint(15, 25) 
-        self.gravity = 0.5
         
-        if self.x < 400:
-            self.velocity_x = randint(2, 5)
+        self.is_bomb = is_bomb
+        if self.is_bomb:
+            self.source = 'assets/images/heart_empty.png'
         else:
-            self.velocity_x = randint(-5, -2)
+            self.source = 'assets/images/heart_full.png'
+            
+        self.size_hint = (None, None)
+        base_size = Window.height * 0.15
+        self.size = (base_size, base_size)
+        
+        screen_w = Window.width
+        self.x = randint(int(screen_w * 0.1), int(screen_w * 0.9))
+        self.y = -self.height
+        
+        target_height = Window.height * (randint(60, 85) / 100.0)
+        distance_to_travel = target_height - self.y
+        
+        speed_multiplier = 1.0 + (difficulty * 0.05)
+        
+        self.gravity = (Window.height * 0.0005) * speed_multiplier
+        
+        self.velocity_y = sqrt(2 * self.gravity * distance_to_travel)
+        
+        x_force = Window.width * 0.005 * speed_multiplier
+        if self.x < screen_w / 2:
+            self.velocity_x = randint(int(x_force * 0.5), int(x_force))
+        else:
+            self.velocity_x = randint(int(-x_force), int(-x_force * 0.5))
             
         self.angle = 0
-        self.rotation_speed = randint(-5, 5)
+        self.rotation_speed = randint(-3, 3) * speed_multiplier
         
         with self.canvas.before:
             PushMatrix()
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import math
+from random import randint, random
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
@@ -7,7 +8,7 @@ from kivy.graphics import Color, Mesh
 from kivy.clock import Clock
 from game_objects import FallingItem
 
-Window.size = (800, 450)
+
 
 Builder.load_file('mookata.kv')
 
@@ -19,25 +20,47 @@ class SettingsScreen(Screen):
 
 class GameScreen(Screen):
     game_objects = []
+    time_elapsed = 0
 
     def on_enter(self):
-        self.game_objects = [] 
+        self.game_objects = []
+        self.time_elapsed = 0
         Clock.schedule_interval(self.game_loop, 1.0/60.0)
-        Clock.schedule_interval(self.spawn_item, 2.0)
+        self.spawn_next_item(0)
 
     def on_leave(self):
         Clock.unschedule(self.game_loop)
-        Clock.unschedule(self.spawn_item)
+        Clock.unschedule(self.spawn_next_item)
         for obj in self.game_objects:
             self.remove_widget(obj)
         self.game_objects.clear()
 
-    def spawn_item(self, dt):
-        item = FallingItem()
-        self.add_widget(item)
-        self.game_objects.append(item)
+    def spawn_next_item(self, dt):
+        difficulty_level = self.time_elapsed / 10.0
+        
+        spawn_count = 1
+        if difficulty_level > 1:
+            spawn_count = randint(1, 2)
+        if difficulty_level > 3:
+            spawn_count = randint(2, 4)
+        if difficulty_level > 5:
+            spawn_count = randint(3, 6)
+            
+        for _ in range(spawn_count):
+            is_bomb = False
+            if difficulty_level > 0.5 and random() < 0.3:
+                is_bomb = True
+            
+            item = FallingItem(difficulty=difficulty_level, is_bomb=is_bomb)
+            self.add_widget(item)
+            self.game_objects.append(item)
+        
+        base_delay = max(0.8, 2.5 - (self.time_elapsed * 0.05))
+        next_spawn_delay = base_delay + (randint(-3, 3) * 0.1)
+        Clock.schedule_once(self.spawn_next_item, next_spawn_delay)
 
     def game_loop(self, dt):
+        self.time_elapsed += dt
         for item in self.game_objects[:]:
             item.update()
             if item.y < -200:
@@ -54,53 +77,31 @@ class GameScreen(Screen):
         return super().on_touch_down(touch)
 
     def on_touch_move(self, touch):
-        if 'trail' not in touch.ud:
-            return super().on_touch_move(touch)
-            
+        if 'trail' not in touch.ud: return super().on_touch_move(touch)
         last_x, last_y = touch.ud['trail'][-1]
         if math.hypot(touch.x - last_x, touch.y - last_y) > 15:
             touch.ud['trail'].append((touch.x, touch.y))
-            if len(touch.ud['trail']) > 15:
-                touch.ud['trail'].pop(0)
+            if len(touch.ud['trail']) > 15: touch.ud['trail'].pop(0)
             self.update_slash(touch)
         return super().on_touch_move(touch)
 
     def update_slash(self, touch):
         trail = touch.ud['trail']
-        if len(trail) < 2:
-            return
-            
-        v_glow, v_core = [], []
-        indices = []
-        
+        if len(trail) < 2: return
+        v_glow, v_core, indices = [], [], []
         for i in range(len(trail)):
             x, y = trail[i]
             progress = i / (len(trail) - 1)
-            
             curve = math.sin((progress ** 2) * math.pi)
-            
             thick_glow = (curve * 18) + (4 * (1 - progress))
             thick_core = (curve * 5) + (1.5 * (1 - progress))
-            
-            if i < len(trail) - 1:
-                dx = trail[i+1][0] - x
-                dy = trail[i+1][1] - y
-            else:
-                dx = x - trail[i-1][0]
-                dy = y - trail[i-1][1]
-                
+            if i < len(trail) - 1: dx, dy = trail[i+1][0] - x, trail[i+1][1] - y
+            else: dx, dy = x - trail[i-1][0], y - trail[i-1][1]
             length = math.hypot(dx, dy)
-            if length == 0:
-                px, py = 0, 0
-            else:
-                px, py = -dy / length, dx / length
-            
-            v_glow.extend([x + px * thick_glow, y + py * thick_glow, 0, 0, x - px * thick_glow, y - py * thick_glow, 0, 0])
-            v_core.extend([x + px * thick_core, y + py * thick_core, 0, 0, x - px * thick_core, y - py * thick_core, 0, 0])
-            
-            idx = i * 2
-            indices.extend([idx, idx+1])
-            
+            px, py = (-dy/length, dx/length) if length else (0,0)
+            v_glow.extend([x+px*thick_glow, y+py*thick_glow, 0, 0, x-px*thick_glow, y-py*thick_glow, 0, 0])
+            v_core.extend([x+px*thick_core, y+py*thick_core, 0, 0, x-px*thick_core, y-py*thick_core, 0, 0])
+            indices.extend([i*2, i*2+1])
         touch.ud['mesh_glow'].vertices = v_glow
         touch.ud['mesh_glow'].indices = indices
         touch.ud['mesh_core'].vertices = v_core
@@ -119,12 +120,10 @@ class GameScreen(Screen):
             self.ids.life_1.source = 'assets/images/heart_full.png'
         else:
             self.ids.life_1.source = 'assets/images/heart_empty.png'
-
         if current_lives >= 2:
             self.ids.life_2.source = 'assets/images/heart_full.png'
         else:
             self.ids.life_2.source = 'assets/images/heart_empty.png'
-
         if current_lives >= 3:
             self.ids.life_3.source = 'assets/images/heart_full.png'
         else:
